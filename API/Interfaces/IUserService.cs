0552472afb7a7909d63fbd006f4483e91d453e0c
n-are-n <naren000000000@gmail.com>
2025-01-30 17:31:16 +0530

    M-IUserService

--- API/Interfaces/IUserService.cs
+++ API/Interfaces/IUserService.cs
@@ -3,6 +3,6 @@ using API.DTOs;
 namespace API.Interfaces;
 public interface IUserService
 {
-    public Task<User> Get(UserDTO user);
+    public /*Task<*/ User /*>*/ Get(UserDTO user);
     public Task<string?> Post(User user);
 }
